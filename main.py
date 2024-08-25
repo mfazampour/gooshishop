299d3f162d37d72fa47ee6981c1c250a72c3e18d
Farid <mf.azampour@tum.de>
2024-08-25 15:38:16 +0200

    added the first plots

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import arabic_reshaper
 from bidi.algorithm import get_display
 from matplotlib import font_manager
 import numpy as np
+import matplotlib.colors as mcolors
 
 def read_data(file_path, sheet_name=None):
     """Reads the Excel file and returns the specified sheet or all sheets as a dictionary."""
@@ -77,63 +78,23 @@ def process_price_data(price_data_df):
 
     plt.show()
 
-    # Filter data for the Digikala channel
-    digikala_df = filtered_df[filtered_df['کانال فروش'] == 'digikala']
+    plots_for_digikala_channel(filtered_df)
 
-    # Ensure the necessary columns exist
-    if 'قیمت فروش' in digikala_df.columns and 'قیمت دیجی' in digikala_df.columns and 'قیمت دیجی اکسترا' in digikala_df.columns:
-        # Calculate the percentage differences
-        digikala_df['perc_diff_قیمت_فروش_دیجی'] = 100 * (digikala_df['قیمت فروش'] - digikala_df['قیمت دیجی']) / \
-                                                  digikala_df['قیمت دیجی']
-        digikala_df['perc_diff_قیمت_فروش_دیجی_اکسترا'] = 100 * (
-                    digikala_df['قیمت فروش'] - digikala_df['قیمت دیجی اکسترا']) / digikala_df['قیمت دیجی اکسترا']
+    plots_gooshi_shop_channel(filtered_df)
 
-        # Create histograms
-        plt.figure(figsize=(10, 5))
 
-        plt.subplot(1, 2, 1)
-        plt.hist(digikala_df['perc_diff_قیمت_فروش_دیجی'], bins=20, color='blue', edgecolor='black')
-        plt.title(get_display(arabic_reshaper.reshape('درصد اختلاف قیمت فروش و قیمت دیجی')),
-                  fontdict={'fontname': 'XB Zar', 'fontsize': 14})
-        plt.xlabel(get_display(arabic_reshaper.reshape('درصد اختلاف قیمت')))
-        plt.ylabel(get_display(arabic_reshaper.reshape('تعداد')))
-
-        plt.subplot(1, 2, 2)
-        plt.hist(digikala_df['perc_diff_قیمت_فروش_دیجی_اکسترا'], bins=20, color='green', edgecolor='black')
-        plt.title(get_display(arabic_reshaper.reshape('درصد اختلاف قیمت فروش و قیمت دیجی اکسترا')),
-                  fontdict={'fontname': 'XB Zar', 'fontsize': 14})
-        plt.xlabel(get_display(arabic_reshaper.reshape('درصد اختلاف قیمت')))
-        plt.ylabel(get_display(arabic_reshaper.reshape('تعداد')))
-
-        plt.tight_layout()
-        plt.show()
-    else:
-        print("Error: Required columns for Digikala price comparisons are missing.")
-
-        # Filter data for the 'گوشی شاپ' channel
-        goshi_shop_df = filtered_df[filtered_df['کانال فروش'] == 'گوشی شاپ']
-
-        # Ensure the necessary column exists
-        if 'رتبه ترب' in goshi_shop_df.columns:
-            # Count the occurrences of each unique value in the 'رتبه ترب' column
-            roteba_torob_counts = goshi_shop_df['رتبه ترب'].value_counts()
-
-            # Create a pie chart
-            plt.figure(figsize=(8, 8))
-            plt.pie(roteba_torob_counts, labels=roteba_torob_counts.index, autopct='%1.1f%%', startangle=140)
-
-            # Set the title with the correct Farsi text
-            plt.title(get_display(arabic_reshaper.reshape('توزیع رتبه ترب برای گوشی شاپ')),
-                      fontdict={'fontname': 'XB Zar', 'fontsize': 16})
-
-            plt.show()
-        else:
-            print("Error: The column 'رتبه ترب' is missing from the گوشی شاپ channel data.")
-
+def plots_gooshi_shop_channel(filtered_df):
     # Filter data for the 'گوشی شاپ' channel
     goshi_shop_df = filtered_df[filtered_df['کانال فروش'] == 'gooshishop']
-
     # Ensure the necessary column exists
+    plot_gs_torob_pie_chart(goshi_shop_df)
+    # Ensure the necessary columns exist
+    goshi_shop_df_clean = plot_gs_torob_comparison(goshi_shop_df)
+
+    plot_gs_torob_2d_hist(goshi_shop_df, goshi_shop_df_clean)
+
+
+def plot_gs_torob_pie_chart(goshi_shop_df):
     if 'رتبه ترب' in goshi_shop_df.columns:
         # Convert values to numeric where possible and group values larger than 10
         roteba_torob_counts = goshi_shop_df['رتبه ترب'].apply(pd.to_numeric, errors='coerce')
@@ -163,12 +124,51 @@ def process_price_data(price_data_df):
     else:
         print("Error: The column 'رتبه ترب' is missing from the گوشی شاپ channel data.")
 
+
+def plot_gs_torob_2d_hist(goshi_shop_df: pd.DataFrame, goshi_shop_df_clean):
+    # Step 1: Preprocess the `roteba_torob_counts` values
+    # Create a new column in goshi_shop_df_clean with specific replacements
+    goshi_shop_df = goshi_shop_df.copy()
+    # Replace +adv with -1
+    goshi_shop_df['roteba_torob'] = goshi_shop_df['رتبه ترب'].replace({'adv': -1, '+30': 11})
+    goshi_shop_df.loc[:, 'roteba_torob'] = goshi_shop_df['roteba_torob'].apply(pd.to_numeric, errors='coerce')
+    # Cap values equal to or greater than 10 at 10
+    goshi_shop_df.loc[:, 'roteba_torob'] = goshi_shop_df['roteba_torob'].apply(lambda x: 10 if x >= 10 else x)
+    goshi_shop_df_clean = goshi_shop_df_clean.merge(goshi_shop_df[['#', 'roteba_torob']], on='#', how='left')
+    # Remove any rows where `roteba_torob` is NaN
+    goshi_shop_df_clean = goshi_shop_df_clean.dropna(subset=['roteba_torob'])
+    # Cap the percentage differences to the range -20% to 20%
+    goshi_shop_df_clean['perc_diff_قیمت_فروش_قیمت_اول_ترب'] = goshi_shop_df_clean[
+        'perc_diff_قیمت_فروش_قیمت_اول_ترب'].apply(lambda x: max(min(x, 20), -20))
+    # Step 2: Create the 2D histogram
+    plt.figure(figsize=(10, 8))
+    hist, xedges, yedges, _ = plt.hist2d(goshi_shop_df_clean['roteba_torob'],
+                                         goshi_shop_df_clean['perc_diff_قیمت_فروش_قیمت_اول_ترب'], bins=[11, 50],
+                                         cmap='plasma', norm=mcolors.Normalize(vmin=0, vmax=35))
+    # Add colorbar for reference
+    plt.colorbar(label=get_display(arabic_reshaper.reshape('تعداد')))
+    # Set titles and labels with Farsi text
+    plt.title(get_display(arabic_reshaper.reshape('ارتباط بین رتبه ترب و درصد اختلاف قیمت')),
+              fontdict={'fontname': 'XB Zar', 'fontsize': 16})
+    plt.xlabel(get_display(arabic_reshaper.reshape('رتبه ترب')), fontdict={'fontname': 'XB Zar', 'fontsize': 14})
+    plt.ylabel(get_display(arabic_reshaper.reshape('درصد اختلاف قیمت')),
+               fontdict={'fontname': 'XB Zar', 'fontsize': 14})
+    # Customize the X-axis labels to replace -1 with (+adv)
+    x_ticks_labels = [get_display(arabic_reshaper.reshape('(+adv)')) if x == -1 else int(x) for x in xedges[:-1]]
+    plt.xticks(xedges[:-1], x_ticks_labels)
+    plt.grid(True, linestyle='--', alpha=0.6)
+    plt.tight_layout()
+    plt.show()
+
+
+def plot_gs_torob_comparison(goshi_shop_df):
     # Ensure the necessary columns exist
-    # Ensure the necessary columns exist
+    goshi_shop_df = goshi_shop_df.copy()
+
     if 'قیمت فروش' in goshi_shop_df.columns and 'قیمت اول ترب' in goshi_shop_df.columns:
         # Calculate the percentage difference
-        goshi_shop_df['perc_diff_قیمت_فروش_قیمت_اول_ترب'] = 100 * (
-                    goshi_shop_df['قیمت فروش'] - goshi_shop_df['قیمت اول ترب']) / goshi_shop_df['قیمت اول ترب']
+        goshi_shop_df.loc[:, 'perc_diff_قیمت_فروش_قیمت_اول_ترب'] = 100 * (
+                goshi_shop_df['قیمت فروش'] - goshi_shop_df['قیمت اول ترب']) / goshi_shop_df['قیمت اول ترب']
 
         # Drop rows with infinite or NaN percentage differences
         goshi_shop_df_clean = goshi_shop_df.replace([np.inf, -np.inf], np.nan).dropna(
@@ -195,48 +195,42 @@ def process_price_data(price_data_df):
         plt.show()
     else:
         print("Error: The necessary columns 'قیمت فروش' and 'قیمت اول ترب' are missing from the گوشی شاپ channel data.")
+    return goshi_shop_df_clean
 
-    # Step 1: Preprocess the `roteba_torob_counts` values
-    goshi_shop_df_clean['roteba_torob'] = goshi_shop_df_clean['رتبه ترب'].apply(pd.to_numeric, errors='coerce')
 
-    # Replace +adv with -1
-    goshi_shop_df_clean['roteba_torob'] = goshi_shop_df_clean['roteba_torob'].fillna(-1)
+def plots_for_digikala_channel(filtered_df):
+    # Filter data for the Digikala channel
+    digikala_df = filtered_df[filtered_df['کانال فروش'] == 'digikala'].copy()
+    # Ensure the necessary columns exist
+    if 'قیمت فروش' in digikala_df.columns and 'قیمت دیجی' in digikala_df.columns and 'قیمت دیجی اکسترا' in digikala_df.columns:
+        # Calculate the percentage differences
+        digikala_df['perc_diff_قیمت_فروش_دیجی'] = 100 * (digikala_df['قیمت فروش'] - digikala_df['قیمت دیجی']) / \
+                                                  digikala_df['قیمت دیجی']
+        digikala_df['perc_diff_قیمت_فروش_دیجی_اکسترا'] = 100 * (
+                digikala_df['قیمت فروش'] - digikala_df['قیمت دیجی اکسترا']) / digikala_df['قیمت دیجی اکسترا']
 
-    # Cap values equal to or greater than 10 at 10
-    goshi_shop_df_clean['roteba_torob'] = goshi_shop_df_clean['roteba_torob'].apply(lambda x: 10 if x >= 10 else x)
+        # Create histograms
+        plt.figure(figsize=(10, 5))
 
-    # Remove any rows where `roteba_torob` is NaN
-    goshi_shop_df_clean = goshi_shop_df_clean.dropna(subset=['roteba_torob'])
+        plt.subplot(1, 2, 1)
+        plt.hist(digikala_df['perc_diff_قیمت_فروش_دیجی'], bins=20, color='blue', edgecolor='black')
+        plt.title(get_display(arabic_reshaper.reshape('درصد اختلاف قیمت فروش و قیمت دیجی')),
+                  fontdict={'fontname': 'XB Zar', 'fontsize': 14})
+        plt.xlabel(get_display(arabic_reshaper.reshape('درصد اختلاف قیمت')))
+        plt.ylabel(get_display(arabic_reshaper.reshape('تعداد')))
 
-    # Cap the percentage differences to the range -20% to 20%
-    goshi_shop_df_clean['perc_diff_قیمت_فروش_قیمت_اول_ترب'] = goshi_shop_df_clean[
-        'perc_diff_قیمت_فروش_قیمت_اول_ترب'].apply(lambda x: max(min(x, 20), -20))
+        plt.subplot(1, 2, 2)
+        plt.hist(digikala_df['perc_diff_قیمت_فروش_دیجی_اکسترا'], bins=20, color='green', edgecolor='black')
+        plt.title(get_display(arabic_reshaper.reshape('درصد اختلاف قیمت فروش و قیمت دیجی اکسترا')),
+                  fontdict={'fontname': 'XB Zar', 'fontsize': 14})
+        plt.xlabel(get_display(arabic_reshaper.reshape('درصد اختلاف قیمت')))
+        plt.ylabel(get_display(arabic_reshaper.reshape('تعداد')))
 
-    # Step 2: Create the 2D histogram
-    plt.figure(figsize=(10, 8))
-    hist, xedges, yedges, _ = plt.hist2d(goshi_shop_df_clean['roteba_torob'],
-                                         goshi_shop_df_clean['perc_diff_قیمت_فروش_قیمت_اول_ترب'], bins=[10, 50],
-                                         cmap='plasma')
+        plt.tight_layout()
+        plt.show()
+    else:
+        print("Error: Required columns for Digikala price comparisons are missing.")
 
-    # Add colorbar for reference
-    plt.colorbar(label=get_display(arabic_reshaper.reshape('تعداد')))
-
-    # Set titles and labels with Farsi text
-    plt.title(get_display(arabic_reshaper.reshape('ارتباط بین رتبه ترب و درصد اختلاف قیمت')),
-              fontdict={'fontname': 'XB Zar', 'fontsize': 16})
-    plt.xlabel(get_display(arabic_reshaper.reshape('رتبه ترب')), fontdict={'fontname': 'XB Zar', 'fontsize': 14})
-    plt.ylabel(get_display(arabic_reshaper.reshape('درصد اختلاف قیمت')),
-               fontdict={'fontname': 'XB Zar', 'fontsize': 14})
-
-    # Customize the X-axis labels to replace -1 with (+adv)
-    x_ticks_labels = [get_display(arabic_reshaper.reshape('(+adv)')) if x == -1 else int(x) for x in xedges[:-1]]
-    plt.xticks(xedges[:-1], x_ticks_labels)
-
-    plt.grid(True, linestyle='--', alpha=0.6)
-    plt.tight_layout()
-    plt.show()
-
-    save_filtered_data(filtered_df, 'filtered_price_data.xlsx')
 
 def save_filtered_data(filtered_df, file_name):
     try:
